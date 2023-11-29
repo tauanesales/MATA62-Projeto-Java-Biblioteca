e6c8643dc823cf3683a933a2f4dc12f6fea9a424
Thales B. Rodrigues <thalesbrito05@gmail.com>
2023-11-28 22:43:58 -0300

    feat: implementa restrições de empréstimo para alunos

--- src/Biblioteca/Biblioteca.java
+++ src/Biblioteca/Biblioteca.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 
 import BancoDeDados.IBancoDeDados;
+import Usuarios.AlunoGraduacao;
+import Usuarios.AlunoPosGraduacao;
 import Usuarios.Usuario;
 
 public class Biblioteca {
@@ -31,26 +33,40 @@ public class Biblioteca {
             return;
         }
 
-        if (usuario != null && exemplar != null && exemplar.isDisponivel() && !temAtraso(usuario)) {
+        string prefixoSucesso = "Empréstimo realizado com sucesso para " + usuario.getNome() + " - Livro: " + exemplar.getTitulo();
+        string prefixoErro = "Não foi possível realizar o empréstimo para " + usuario.getNome() + " - Livro: " + exemplar.getTitulo();
+
+        boolean atrasado = temAtraso(usuario);
+
+        if (exemplar.isDisponivel() && !atrasado) {
+            if (usuario instanceof Aluno) {
+                int numEmprestimos = contarEmprestimosAbertos(usuario);
+                int maxEmprestimos = 
+                    usuario instanceof AlunoPosGraduacao ? 4 
+                    : usuario instanceof AlunoGraduacao ? 3
+                    : 0;
+
+                if (numEmprestimos >= maxEmprestimos) {
+                    System.out.println(prefixoErro + " Usuário não pode ter mais que " + maxEmprestimos + " empréstimos em aberto simultaneamente.");
+                }
+            }
+
             Date dataSolicitacao = new Date();
             Date dataDevolucao = calcularDataDevolucao();
 
             Emprestimo emprestimo = new Emprestimo(exemplar, usuario, dataSolicitacao, dataDevolucao);
             this.bancoDeDados.add(emprestimo);
             exemplar.setDisponivel(false);
-            System.out.println("Empréstimo realizado com sucesso para " + usuario.getNome() +
-                    " - Livro: " + exemplar.getTitulo() +
-                    " - Devolução até: " + formatarData(dataDevolucao));
+            System.out.println(prefixoSucesso + " Devolução até: " + formatarData(dataDevolucao));
         } else {
-            System.out.println("Não foi possível realizar o empréstimo para " + usuario.getNome() +
-                    " - Livro: " + exemplar.getTitulo() +
+            System.out.println(prefixoErro +
                     (exemplar.isDisponivel() ? " Livro indisponível." : "") +
-                    (temAtraso(usuario) ? " Usuário com atraso em devolução." : ""));
+                    (atrasado ? " Usuário com atraso em devolução." : ""));
         }
     }
 
     private boolean temAtraso(Usuario usuario) {
-        List<Emprestimo> emprestimosUsuario = obterEmprestimosPorUsuario(usuario);
+        List<Emprestimo> emprestimosUsuario = obterEmprestimosPorUsuario(usuario, true);
         Date dataAtual = new Date();
 
         for (Emprestimo emprestimo : emprestimosUsuario) {
@@ -62,12 +78,20 @@ public class Biblioteca {
         return false; // Usuário não tem empréstimos em atraso
     }
 
-    private List<Emprestimo> obterEmprestimosPorUsuario(Usuario usuario) {
+    private int contarEmprestimosAbertos(Usuario usuario) {
+        List<Emprestimo> emprestimosUsuario = obterEmprestimosPorUsuario(usuario, true);
+        return emprestimosUsuario.size();
+    }
+
+    private List<Emprestimo> obterEmprestimosPorUsuario(Usuario usuario, boolean apenasEmAberto) {
         List<Emprestimo> emprestimosUsuario = new ArrayList<>();
         List<? extends IEntidadeBiblioteca> entidades = bancoDeDados.getAll(Emprestimo.class);
         for (IEntidadeBiblioteca entidade : entidades) {
-            if (entidade instanceof Emprestimo && ((Emprestimo) entidade).getUsuario().equals(usuario)) {
-                emprestimosUsuario.add((Emprestimo) entidade);
+            if (entidade instanceof Emprestimo) {
+                Emprestimo emprestimo = (Emprestimo) entidade;
+                if (emprestimo.getUsuario().equals(usuario) && (!apenasEmAberto || !emprestimo.isDevolvido())) {   
+                    emprestimosUsuario.add(emprestimo);
+                }
             }
         }
         return emprestimosUsuario;
