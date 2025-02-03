cc66f0bd0804c781a554b8f237dd253f2918b30f
Mayara <129678256+mayara-santos01@users.noreply.github.com>
2025-02-03 13:02:23 -0300

    Create ReservaBancoDeDados.java

---
+++ TRABALHO/src/main/java/TRABALHO/BancoDeDados/ReservaBancoDeDados.java
@@ -0,0 +1,33 @@
+public class ReservaBancoDeDados implements IReservaBancoDeDados {
+    private BancoDeDados bancoDeDados;
+
+    public ReservaBancoDeDados(BancoDeDados bancoDeDados) {
+        this.bancoDeDados = bancoDeDados;
+    }
+
+    @Override
+    public List<Reserva> getReservasPorCodigoUsuario(boolean b, int codigoUsuario) {
+        return this.bancoDeDados.getAll(Reserva.class)
+                .stream()
+                .filter(reserva -> reserva.getUsuario().getCodigo() == codigoUsuario)
+                .filter(reserva -> !b || reserva.reservaEstaAtiva())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Reserva getReservaAtiva(int codigoLivro, int codigoUsuario) {
+        return this.bancoDeDados.getAll(Reserva.class)
+                .stream()
+                .filter(reserva -> reserva.getUsuario().getCodigo() == codigoUsuario)
+                .filter(reserva -> reserva.getLivro().getCodigo() == codigoLivro)
+                .filter(reserva -> reserva.reservaEstaAtiva())
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Reserva> getReservasPorCodigoLivro(boolean reservaAtiva, int codigoLivro) {
+        return this.bancoDeDados.getAll(Reserva.class)
+                .stream()
+                .filter(reserva);
+          }
