33067c553dd009f4702f6fb04f586c1e4064c8e7
Mayara <129678256+mayara-santos01@users.noreply.github.com>
2025-02-04 21:23:07 -0300

    Update and rename IUsuarioAluno.java to IUsuario.java

--- TRABALHO/src/main/java/TRABALHO/Usuarios/IUsuario.java
+++ TRABALHO/src/main/java/TRABALHO/Usuarios/IUsuario.java
@@ -6,7 +6,7 @@ import TRABALHO.Emprestimo.Emprestimo;
 import TRABALHO.Reserva.Reserva;
 import TRABALHO.SistemaBiblioteca.IEntidadeBiblioteca;
 
-public interface IUsuarioAluno extends IEntidadeBiblioteca {
+public interface IUsuario extends IEntidadeBiblioteca {
     public String getNome();
 
     public boolean temAtraso();
@@ -37,13 +37,10 @@ public interface IUsuarioAluno extends IEntidadeBiblioteca {
 
     public void notificar();
 
-    //No momento, essa classe é usada somente pelo professor, mas foi mantida pela possbilidade
-    //de outros tipos de usuario usarem, como os alunos
     default Boolean podeSerObservador() {
         return false;
     }
-
-    //Essa classe é mantida, pois precisamos verificar se há possibilidade de priorização de interfaces
+    
     default Boolean podeIgnorarListaDeReservas() {
         return false;
     }
